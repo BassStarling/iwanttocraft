6c1139472ebd72dfde985a4db54ca3a9ac4cb0f0
BassStraling <bassstarling@outlook.com>
2024-12-14 22:24:01 +0900

    item追加中

---
+++ src/main/java/org/craftcore/iwanttocraft/item/iwanttocraftitems.java
@@ -0,0 +1,13 @@
+package org.craftcore.iwanttocraft.item;
+
+import net.minecraft.world.item.Item;
+import net.minecraftforge.common.Tags;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.server.command.ModIdArgument;
+
+public class iwanttocraftitems {
+    // レジストリを作成
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, iwanttocraftitems.MODID) ;
+
+}
